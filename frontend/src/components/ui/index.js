43be37b6d2858ce9a9d5e96750eab194b717d088
saifullah-siddique <saifullahshasiddiqui@gmail.com>
2026-02-14 01:58:04 +0530

    intialized Barrel Export for ui component

---
+++ frontend/src/components/ui/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button";
+export { default as FeatureCard } from "./FeatureCard";
+export { default as InputField } from "./InputField";
+export { default as StatBox } from "./StatBox"
+export { default as Loader } from "./Loader"
+
